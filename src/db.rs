3884470018f635e64b68d9d8392eca31b25441a1
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-02-12 18:11:29 +0200

    initial pool implementation

---
+++ src/db.rs
@@ -0,0 +1,42 @@
+use std::ops::Deref;
+
+use r2d2;
+use redis::Connection;
+use r2d2_redis::RedisConnectionManager;
+
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+
+pub type Pool = r2d2::Pool<RedisConnectionManager>;
+
+pub const DATABASE_URL: &'static str = env!("REDISCLOUD_URL");
+
+pub fn init_pool() -> Pool {
+    let manager = RedisConnectionManager::new(DATABASE_URL).unwrap();
+    r2d2::Pool::builder()
+        .build(manager).expect("db pool")
+}
+
+pub struct Conn(pub r2d2::PooledConnection<RedisConnectionManager>);
+
+impl Deref for Conn {
+    type Target = Connection;
+
+    #[inline(always)]
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Conn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Conn, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(Conn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,43 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+extern crate redis;
+extern crate r2d2;
+extern crate r2d2_redis;
+
+mod db;
+
+use std::io::Cursor;
+use r2d2_redis::RedisConnectionManager;
+use rocket::*;
+use rocket::http::*;
+use rocket::response::*;
 
 #[get("/")]
-fn index() -> &'static str {
-    r#"{"goto":"http://www.iron.io"}"#
+fn index() -> String {
+    String::from(r#"{"goto":"http://www.iron.io"}"#)
+}
+
+#[get("/version")]
+fn redis_version(conn: db::Conn) -> String {
+    let info : redis::InfoDict = redis::cmd("INFO").query(&*conn).unwrap();
+    let version : Option<String> = info.get("redis_version");
+
+    format!("Redis version: {} ", version.unwrap())
+}
+
+fn rocket() -> (Rocket, Option<db::Conn>) {
+    let pool = db::init_pool();
+    let conn = Some(db::Conn(pool.get().expect("database connection")));
+
+    let rocket = rocket::ignite()
+        .manage(pool)
+        .mount("/", routes![index])
+        .mount("/redis/", routes![redis_version]);
+
+    (rocket, conn)
 }
 
 fn main() {
-    rocket::ignite()
-        .mount("/", routes![index])
-        .launch();
+    rocket().0.launch();
 }
