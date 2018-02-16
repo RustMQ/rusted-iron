053d37d3da4e7bb7a39a719f690dc034279d5aa7
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-02-16 15:47:35 +0200

    add post new message endpoint
    redis pipline experiment

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ extern crate r2d2;
 extern crate r2d2_redis;
 extern crate serde;
 extern crate serde_json;
-
+extern crate rand;
+extern crate uuid;
 #[macro_use]
 extern crate rocket_contrib;
 #[macro_use]
@@ -17,13 +18,17 @@ mod db;
 
 use rocket::{Rocket};
 use rocket::http::{RawStr};
-use rocket_contrib::{Json};
+use rocket_contrib::{Json, Value};
 use redis::RedisError;
+use rand::{thread_rng, Rng};
+use uuid::Uuid;
 
 #[get("/")]
 fn index() -> String {
     String::from(r#"{"goto":"http://www.iron.io"}"#)
 }
+
+const DEFAULT_QUEUE: &'static str = "queue:Q";
 
 #[get("/version")]
 fn redis_version(conn: db::Conn) -> Json {
@@ -36,14 +41,49 @@ fn redis_version(conn: db::Conn) -> Json {
 }
 
 #[get("/message/<key>")]
-fn redis_get_key(key: &RawStr, conn: db::Conn) -> Json {
+fn redis_get_key(key: &RawStr, conn: db::Conn) -> Json<Value> {
     let key_as_str = key.as_str();
-    let result: Result<String, RedisError> = redis::Cmd::new().arg("GET").arg(key_as_str).query(&*conn);
+    let result: Result<String, RedisError> = redis::Cmd::new().arg("HGET").arg(DEFAULT_QUEUE).arg(key_as_str).query(&*conn);
     match result {
         Ok(v) => {
             return Json(json!({
                 "key": key_as_str.to_string(),
                 "value": String::from(v)
+            }))
+        },
+        Err(e) => {
+            return Json(json!({
+                "status": "error",
+                "reason": format!("Internal Server Error: {}", e)
+            }))
+        },
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+    body: String
+}
+
+#[post("/message", format = "application/json", data = "<message>")]
+fn redis_new_message(message: Json<Message>, conn: db::Conn) -> Json<Value> {
+    let s: String = thread_rng().gen_ascii_chars().take(10).collect();
+    println!("{}", s);
+    let totalrecv: u64 = redis::Cmd::new().arg("HGET").arg(DEFAULT_QUEUE).arg("totalrecv").query(&*conn).unwrap();
+    println!("Total message in queur: {}", totalrecv );
+    let mid = Uuid::new_v5(&uuid::NAMESPACE_DNS, &s);
+    println!("{}", mid);
+    let result: Result<Vec<u64>, RedisError> = redis::pipe()
+        .cmd("zadd").arg("queue").arg(totalrecv).arg(s.clone()).ignore()
+        .cmd("hset").arg(DEFAULT_QUEUE).arg(s.clone()).arg(mid.hyphenated().to_string()).ignore()
+        .cmd("hincrby").arg(DEFAULT_QUEUE).arg("totalrecv").arg(1).query(&*conn);
+
+    match result {
+        Ok(v) => {
+            println!("Messge: {:?}", v);
+            return Json(json!({
+                "mid": s.clone(),
+                "status": "ok"
             }))
         },
         Err(e) => {
@@ -71,7 +111,7 @@ fn rocket() -> (Rocket, Option<db::Conn>) {
     let rocket = rocket::ignite()
         .manage(pool)
         .mount("/", routes![index])
-        .mount("/redis/", routes![redis_version, redis_get_key])
+        .mount("/redis/", routes![redis_version, redis_get_key, redis_new_message])
         .catch(errors![not_found]);
 
     (rocket, conn)
