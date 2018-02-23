529866cc703f7686b9ddd895e5febe58927f59c7
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-02-23 17:32:06 +0200

    wrap logic into transaction context

--- src/message.rs
+++ src/message.rs
@@ -13,33 +13,49 @@ const MESSAGE_PART_KEY: &'static str = "msg";
 const MESSAGE_GEN_PART_KEY: &'static str = "counter";
 
 impl Message {
-    pub fn push_message(queue: &Queue, message: &Message, _con: &Connection) -> i32 {
+    pub fn push_message(queue: &Queue, message: &Message, con: &Connection) -> i32 {
         println!("Message: {:?}", message);
-        let mid: i32 = cmd("GET").arg("queue:1:msg:counter").query(_con).unwrap();
-        let mut key = String::new();
-        key.push_str("queue:1:msg:");
-        key.push_str(&mid.to_string());
-        let response: Result<Vec<i32>, RedisError> = redis::pipe()
-            .atomic()
-            .cmd("HSET")
-                .arg(key)
-                .arg("body")
-                .arg(&message.body)
-            .cmd("INCR")
-                .arg("queue:1:msg:counter")
-            .cmd("HINCRBY")
-                .arg("queue:1")
-                .arg("totalrecv")
-                .arg("1")
-                .ignore()
-            .query(_con);
-        match response {
-            Err(e) => print!("{:?}", e),
-            Ok(v) => {
-                println!("V: {:?}", v);
-            }
-        }
+        let msg_counter_key = "queue:1:msg:counter";
+        let queue_key = "queue:1";
+        let mut msg_key = String::new();
+        msg_key.push_str(&queue_key);
+        msg_key.push_str(":msg:");
 
-        1
+        let msg_id = redis::transaction(con, &[msg_counter_key], |pipe| {
+            let msg_id: i32 = cmd("GET").arg(msg_counter_key).query(con).unwrap();
+            msg_key.push_str(&msg_id.to_string());
+
+            let response: Result<Vec<i32>, RedisError> = pipe
+                    .cmd("HSET")
+                        .arg(&msg_key)
+                        .arg("body")
+                        .arg(&message.body)
+                    .cmd("INCR")
+                        .arg("queue:1:msg:counter")
+                        .ignore()
+                    .cmd("HINCRBY")
+                        .arg(queue_key)
+                        .arg("totalrecv")
+                        .arg("1")
+                        .ignore()
+                    .query(con);
+
+            match response {
+                Err(e) => print!("{:?}", e),
+                Ok(v) => {
+                    if v[0] == 1 {
+                        println!("New message set");
+                    }
+                    else {
+                        println!("New message not set");
+                    }
+                    println!("V: {:?}", v);
+                }
+            }
+
+            Ok(Some(msg_id))
+        }).unwrap();
+
+        msg_id
     }
 }
