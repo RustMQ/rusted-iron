12c4e00ed65f72fc4f0f2f0102adda4e6af80d0f
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-03-29 16:22:54 +0300

    added custom thread_pool

--- src/pool/mod.rs
+++ src/pool/mod.rs
@@ -1,5 +1,8 @@
-use std::env;
+extern crate num_cpus;
 
+use std::env;
+use scheduled_thread_pool::ScheduledThreadPool;
+use std::sync::Arc;
 use r2d2;
 use r2d2_redis::RedisConnectionManager;
 
@@ -10,10 +13,13 @@ pub fn new_pool() -> Pool {
     let max_size: String = env::var("REDIS_CONNECTION_MAX_SIZE").expect("$REDIS_CONNECTION_MAX_SIZE is provided");
     info!("REDISCLOUD_URL: {:?}", database_url);
     info!("REDIS_CONNECTION_MAX_SIZE: {:?}", max_size);
+    let threads = num_cpus::get();
+    let thread_pool = ScheduledThreadPool::new(threads);
 
     let manager = RedisConnectionManager::new(&*database_url).unwrap();
     r2d2::Pool::builder()
         .max_size(max_size.parse::<u32>().unwrap())
+        .thread_pool(Arc::new(thread_pool))
         .build(manager)
         .expect("db pool is not created")
 }
