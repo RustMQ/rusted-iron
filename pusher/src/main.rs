55d8fe758158f063eb757d4dc0fc5257f42490b0
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-04-12 11:36:57 +0300

    initial version of msg listener

--- pusher/src/main.rs
+++ pusher/src/main.rs
@@ -1,20 +1,21 @@
-extern crate futures;
-extern crate tokio;
-extern crate tokio_timer;
+extern crate redis;
 
-use tokio::prelude::*;
-use tokio_timer::Interval;
-
-use std::time::{Duration, Instant};
+use redis::Client;
+use std::thread;
 
 fn main() {
-    let task = Interval::new(Instant::now(), Duration::from_millis(100))
-        .take(10)
-        .for_each(|instant| {
-            println!("fire; instant={:?}", instant);
-            Ok(())
-        })
-        .map_err(|e| panic!("interval errored; err={:?}", e));
+    let client = Client::open("redis://localhost:6379/").expect("Failed to obtain client");
+    let mut pubsub = client.get_pubsub().expect("Failed to obtain pub-sub");
+    pubsub.psubscribe("queue:*:msg:channel").expect("Failed to subscribe on channel_1");
 
-    tokio::run(task);
+    loop {
+        let msg = pubsub.get_message().unwrap();
+        let payload : String = msg.get_payload().unwrap();
+        if !payload.is_empty() {
+            let builder = thread::Builder::new();
+            builder.spawn(move || {
+                println!("channel '{}': {}", msg.get_channel_name(), payload);
+            }).unwrap();
+        }
+    }
 }
