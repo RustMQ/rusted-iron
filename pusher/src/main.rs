ba3f93809bf23977b983c990c78e671ae16ba786
Maksym Shekhovets <maximsh@interlink-ua.com>
2018-04-12 12:02:19 +0300

    add configurable DB url

--- pusher/src/main.rs
+++ pusher/src/main.rs
@@ -1,10 +1,11 @@
 extern crate redis;
 
 use redis::Client;
-use std::thread;
+use std::{env, thread};
 
 fn main() {
-    let client = Client::open("redis://localhost:6379/").expect("Failed to obtain client");
+    let database_url = env::var("REDISCLOUD_URL").expect("$REDISCLOUD_URL is provided");
+    let client = Client::open(database_url.as_str()).expect("Failed to obtain client");
     let mut pubsub = client.get_pubsub().expect("Failed to obtain pub-sub");
     pubsub.psubscribe("queue:*:msg:channel").expect("Failed to subscribe on channel_1");
 
